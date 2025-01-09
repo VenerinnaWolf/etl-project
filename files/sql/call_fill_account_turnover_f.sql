fba28d3c6fa15086b06fd9cdbf4073d734d77ff0
Veronika <101918389+VenerinnaWolf@users.noreply.github.com>
2025-01-09 13:46:18 +0300

    Update call_fill_account_turnover_f.sql

--- files/sql/call_fill_account_turnover_f.sql
+++ files/sql/call_fill_account_turnover_f.sql
@@ -1,10 +1,10 @@
---рассчитайте витрину dm.dm_account_turnover_f за каждый день января 2018 года
-BEGIN;
-
-CALL ds.fill_account_turnover_f('2018-01-09');
-SELECT * FROM dm.dm_account_turnover_f;
-CALL ds.fill_account_turnover_f('2018-01-10');
-SELECT * FROM dm.dm_account_turnover_f;
-
-ROLLBACK;
---COMMIT;
+-- Заполнение витрины оборотов по лицевым счетам dm.dm_account_turnover_f за каждый день января 2018 года:
+DO
+$$
+BEGIN
+	FOR i IN 1..31 LOOP
+		CALL ds.fill_account_turnover_f(('2018-01-' || i)::date);
+--		PERFORM pg_sleep(1);
+	END LOOP;
+END
+$$ LANGUAGE plpgsql;
